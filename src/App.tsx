bab5b6f53940e0a7131823d2beff3b0b364dc9ad
nikolai.mashynski <nikolai.mashynski@intexsoft.by>
2021-11-09 03:12:32 +0300

    created basic to do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+let notes: any[] = [];
+
+const App: React.FC = () => {
+    const [count, setCount] = useState(0);
+    const [input, setInput] = useState('');
+
+    const addNote = (value: any) => {
+        notes.push({id: getRandomId(10000, 99999), content: value});
+        setCount(count + 1);
+    }
+
+    const deleteNote = (index: number) => {
+        notes.splice(index,1);
+        setCount(count + 1);
+    }
+
+    return (
+    <div className="main__container">
+        <h1>TO DO LIST</h1>
+        <form className="form">
+            <input
+                type="text"
+                className="add_input"
+                placeholder="Enter note..."
+                value={input} onChange={e => setInput(e.target.value)}
+            />
+            <div className="add_button" onClick={() => addNote(input)}>Add</div>
+        </form>
+        <div className="notes_list" >{notes.map((item) => NoteItem(item, deleteNote))}</div>
     </div>
-  );
+    );
+}
+
+const NoteItem: React.FC = (item: any, deleteNote) => {
+    return (
+        <div className="note_item" key={item.id}>
+            <p>{item.content}</p>
+            <div className="close" onClick={() => deleteNote(notes.indexOf(item))}>x</div>
+        </div>
+    );
+}
+
+const getRandomId = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export default App;
